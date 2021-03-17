f5d0638a313ea5196efdfb7e31307469c1f67164
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-16 21:58:05 -0300

    update

---
+++ ex03/aula11.js
@@ -0,0 +1,6 @@
+const vel = 40
+console.log(`Você passou a ${vel}km/h.`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a valocidade permitida, você foi MULTADO!`)
+}
+console.log(`Dirija sempre com segurança, Boa Viagem!`)
