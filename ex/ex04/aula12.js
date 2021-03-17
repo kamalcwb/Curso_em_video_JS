63c1da909efe9492bf657943951345dbc3d378e2
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-17 00:01:37 -0300

    Update aula12.js

--- ex/ex04/aula12.js
+++ ex/ex04/aula12.js
@@ -15,3 +15,33 @@ if (hora >= 0 && hora < 12) {
 } else if (hora >= 18 && hora <= 24) {
     console.log('Boa noite')
 }
+
+const agora = new Date()
+const dia = agora.getDay()
+
+console.log(dia)
+switch (dia) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda-feira')
+        break
+    case 2:
+        console.log('Terça-feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default:
+        console.log('Dia incorreto')
+}
