3c57684a6c140792862c3974d02743b371d52f80
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-16 22:16:24 -0300

    update

--- ex03/aula11.js
+++ ex03/aula11.js
@@ -1,6 +1,13 @@
-const vel = 40
+/*const vel = 40
 console.log(`Você passou a ${vel}km/h.`)
 if (vel > 60) {
     console.log(`Você ultrapassou a valocidade permitida, você foi MULTADO!`)
 }
-console.log(`Dirija sempre com segurança, Boa Viagem!`)
+console.log(`Dirija sempre com segurança, Boa Viagem!`)*/
+
+const pais = 'br'
+if (pais == 'br') {
+    console.log('A pessoa é brasileira')
+} else {
+    console.log('A pessoa é estrangeira')
+}
