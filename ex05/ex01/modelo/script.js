3456c4b6ab0557d7f22e58d29f5809fa53b201a4
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-18 23:46:32 -0300

    update

--- ex05/ex01/modelo/script.js
+++ ex05/ex01/modelo/script.js
@@ -1,2 +1,16 @@
-const msg = document.getElementById('msg')
-const img = document.getElementById('foto')
+function carregar() {
+    const msg = document.getElementById('msg')
+    const img = document.getElementById('foto')
+    const data = new Date()
+    const hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 18 && hora < 6) {
+        msg.innerHTML = "Agora é noite"
+        img.src = ""
+    } else if (hora >= 6 && hora < 12) {
+        msg.innerHTML = "Agora é manhã"
+    } else if (hora >= 12 && hora < 18) {
+        msg.innerHTML = "Agora é tarde"
+    }
+}
+
