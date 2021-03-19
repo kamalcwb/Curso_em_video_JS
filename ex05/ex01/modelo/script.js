ab179d393b7ac73ef963e3c134ac58c99f6cbbc8
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-19 00:04:50 -0300

    complete

--- ex05/ex01/modelo/script.js
+++ ex05/ex01/modelo/script.js
@@ -5,11 +5,14 @@ function carregar() {
     const hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
+        document.body.style.background = "#7fffd4"
         img.src = "./img/manha.jpg"
     } else if (hora >= 12 && hora < 18) {
         img.src = "./img/tarde.jpg"
+        document.body.style.background = "#d9db4e"
     } else if (hora >= 18 && hora <= 23) {
         img.src = "./img/noite.jpg"
+        document.body.style.background = "#060997"
     }
 }
 
