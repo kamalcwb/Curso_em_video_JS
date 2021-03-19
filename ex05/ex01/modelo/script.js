008ff1dde95cf241091634c2c7e15e9572eedf6a
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-18 23:59:33 -0300

    update

--- ex05/ex01/modelo/script.js
+++ ex05/ex01/modelo/script.js
@@ -1,15 +1,15 @@
 function carregar() {
     const msg = document.getElementById('msg')
-    const img = document.getElementById('foto')
+    const img = document.getElementById('imagem')
     const data = new Date()
     const hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 18 && hora < 6) {
-        img.src = "noite.jpg"
-    } else if (hora >= 6 && hora < 12) {
-        img.src = "manha.jpg"
+    if (hora >= 0 && hora < 12) {
+        img.src = "./img/manha.jpg"
     } else if (hora >= 12 && hora < 18) {
-        img.src = "tarde.jpg"
+        img.src = "./img/tarde.jpg"
+    } else if (hora >= 18 && hora <= 23) {
+        img.src = "./img/noite.jpg"
     }
 }
 
