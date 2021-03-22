e1d2d0fd036f576e197caa29ad3cd4b1aaa97cf2
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-21 21:00:40 -0300

    commit

--- ex/tst.js
+++ ex/tst.js
@@ -3,7 +3,10 @@ var n = '10'
 while (n > 1) {
     let dias = 0
     let k = n
-    k = k / 2
-    dias++
+    while (k > 1) {
+        k = k / 2
+        dias = dias + 1
+    }
+
 }
-console.log(`${n}kilos de comida duram ${dias}`)
+console.log(`${n}kilos de comida duram ${dias} dias`)
