682b9b121f968b14ebdc85fb94fca294f9440768
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-27 14:44:54 -0300

    update

--- ex/ex07/ex01/script.js
+++ ex/ex07/ex01/script.js
@@ -2,4 +2,18 @@ function contar() {
     const ini = document.getElementById('txti')
     const fim = document.getElementById('txtf')
     const passo = document.getElementById('txtp')
+    const res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO]')
+    } else {
+        res.innerHTML = `Contando...`
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
+        }
+    }
 }
---
+++ ex/ex07/ex02/ex02/script.js
@@ -0,0 +1,9 @@
+function tabuada() {
+    let num = document.getElementById("txtn")
+    let tab = document.getElementById("seltab")
+    if (num.value.lenght == 0) {
+        window.alert('Por favor, digite um número.')
+    } else {
+        let n = Number(num.value)
+    }
+}
